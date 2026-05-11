2dda166c6edb29297fa6bd1d8e466d2334dc9a93
kingsleychino <kingsleychinono@gmail.com>
2026-05-11 10:58:33 +0100

    Fix: add default values to all variables

--- app/terraform/website/variables.tf
+++ app/terraform/website/variables.tf
@@ -3,9 +3,22 @@ variable "container_image" {
   type        = string
 }
 
-variable "project_name" {}
-variable "vpc_cidr" {}
-variable "public_subnet_az1_cidr" {}
-variable "public_subnet_az2_cidr" {}
-variable "region" {}
+variable "project_name" {
+  default = "my-app"
+}
 
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_az1_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_az2_cidr" {
+  default = "10.0.2.0/24"
+}
