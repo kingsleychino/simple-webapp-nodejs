eb51b3e8fb37897e0ce3830ca2ca4a2060cfb960
kingsleychino <kingsleychinono@gmail.com>
2026-05-11 10:54:35 +0100

    Fix: declare container_image variable and add tfvars for static values

--- app/terraform/website/variables.tf
+++ app/terraform/website/variables.tf
@@ -1,5 +1,11 @@
+variable "container_image" {
+  description = "ECR image URI passed in from Jenkins"
+  type        = string
+}
+
 variable "project_name" {}
 variable "vpc_cidr" {}
 variable "public_subnet_az1_cidr" {}
 variable "public_subnet_az2_cidr" {}
 variable "region" {}
+
